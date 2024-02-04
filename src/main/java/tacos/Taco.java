fc18e94b13d6c6505effcbe6baa3bca210c93e61
hkkong <konguuu@gmail.com>
2024-02-04 23:25:53 +0900

    JdbcTacoRepository development finish

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -16,5 +16,5 @@ public class Taco {
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
     @Size(min=1, message="You must choose at least 1 ingredient")
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
 }
