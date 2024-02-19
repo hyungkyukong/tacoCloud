239512445886f098e6383b933f17714843205363
hkkong <konguuu@gmail.com>
2024-02-19 22:11:29 +0900

    4.2.2 JDBC 기반의 사용자 스토어 finish

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,31 +1,47 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/design", "/orders").access("hasRole('ROLE_USER')")
-                .antMatchers("/","/**").access("permitAll").and().httpBasic();
+                .antMatchers("/","/**","/h2-console/**").access("permitAll").and().httpBasic();
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
+
+        auth.
+                jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username=?")
+                .passwordEncoder(new NoEncodingPasswordEncoder());
+
+
+
+        /*auth.inMemoryAuthentication()
                 .withUser("user1")
                 .password("{noop}password1")
                 .authorities("ROLE_USER")
                 .and()
                 .withUser("user2")
                 .password("{noop}password2")
-                .authorities("ROLE_USER");
+                .authorities("ROLE_USER");*/
 
     }
 }
